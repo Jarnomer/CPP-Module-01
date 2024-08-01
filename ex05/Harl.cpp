7d2365abf347ab4ddb6a3177c80038967d451bb7
Jarnomer <jankkumer@pm.me>
2024-08-01 22:57:37 +0300

    Updated ex05

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -11,17 +11,15 @@ void Harl::error(void) { cout << "Example error message" << endl; }
 void Harl::complain(string level) {
   void (Harl::*log[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
   string levels[4] = {"DEBUG", "ERROR", "INFO", "WARNING"};
-  int i = 0;
 
-  for (i = 0; i < 4; i++) {
+  cout << level << endl;
+  for (int i = 0; i < 4; i++) {
     if (level == levels[i]) {
+      (this->*log[i])();
+      break;
+    } else if (i == 3) {
+      cout << "Invalid level" << endl;
       break;
     }
-  }
-  cout << level << endl;
-  if (i < 4) {
-    (this->*log[i])();
-  } else {
-    cout << "Invalid level" << endl;
   }
 }
