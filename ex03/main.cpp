625a3a36b8e26aee930df9d2a4cac687781ba971
Jarnomer <jankkumer@pm.me>
2024-08-15 15:52:22 +0300

    Updated ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -2,20 +2,23 @@
 #include "HumanB.hpp"
 
 int main(void) {
-  Weapon club1 = Weapon("crude spiked club");
-  HumanA bob("Bob", club1);
+  {
+    Weapon club1 = Weapon("crude spiked club");
+    HumanA bob("Bob", club1);
 
-  bob.attack();
-  club1.setType("some other type of club");
-  bob.attack();
+    bob.attack();
+    club1.setType("some other type of club");
+    bob.attack();
+  }
+  {
+    Weapon club2 = Weapon("crude spiked club");
+    HumanB jim("Jim");
 
-  Weapon club2 = Weapon("crude spiked club");
-  HumanB jim("Jim");
-
-  jim.setWeapon(club2);
-  jim.attack();
-  club2.setType("some other type of club");
-  jim.attack();
-
+    jim.attack();
+    jim.setWeapon(club2);
+    jim.attack();
+    club2.setType("some other type of club");
+    jim.attack();
+  }
   return 0;
 }
